07cc200f886e0b22f9fe4b2f5265693ae92b08cd
Byron2306 <132565250+Byron2306@users.noreply.github.com>
2026-08-11 19:00:07 +0200

    Add Seraph challenge organ adapter

---
+++ adapters/seraph/__init__.py
@@ -0,0 +1,5 @@
+"""Seraph adversarial/security challenge adapter for DIO."""
+
+from .challenge import build_challenge_receipt, project_challenge_into_case
+
+__all__ = ["build_challenge_receipt", "project_challenge_into_case"]
