abde5682393a63799c7c37d3fdbab12e9dc68486
Byron2306 <132565250+Byron2306@users.noreply.github.com>
2026-08-19 20:30:42 +0200

    Activate M1 LINGUA semantic law package

---
+++ lingua/__init__.py
@@ -0,0 +1,12 @@
+"""DIO LINGUA semantic-law and projection surfaces."""
+
+from lingua.semantic_law import build_semantic_law, validate_projection
+from lingua.product_projection import build_projection_plan, creative_distance, project_story
+
+__all__ = [
+    "build_semantic_law",
+    "validate_projection",
+    "build_projection_plan",
+    "creative_distance",
+    "project_story",
+]
