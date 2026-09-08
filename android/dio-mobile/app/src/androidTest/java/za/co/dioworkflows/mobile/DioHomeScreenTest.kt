3573a305dda30631b1c080982b0144ae696faa7d
Byron2306 <132565250+Byron2306@users.noreply.github.com>
2026-09-08 08:03:16 +0200

    fix: compile DIO Android instrumentation tests

--- android/dio-mobile/app/src/androidTest/java/za/co/dioworkflows/mobile/DioHomeScreenTest.kt
+++ android/dio-mobile/app/src/androidTest/java/za/co/dioworkflows/mobile/DioHomeScreenTest.kt
@@ -1,7 +1,5 @@
 package za.co.dioworkflows.mobile
 
-import androidx.compose.ui.test.assertDoesNotExist
-import androidx.compose.ui.test.assertExists
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.test.ext.junit.runners.AndroidJUnit4
