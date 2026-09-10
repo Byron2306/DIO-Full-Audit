e8e547633bdfb08bf731616f35fe902c3753863b
Byron2306 <132565250+Byron2306@users.noreply.github.com>
2026-09-10 10:11:02 +0200

    feat: export capital discovery adapter primitives

---
+++ market_capital/adapters/__init__.py
@@ -0,0 +1,5 @@
+"""Policy-aware discovery adapter interfaces for the Capital & Support census."""
+
+from .base import DiscoveryBatch, DiscoveryObservation, SourceAdapter
+
+__all__ = ["DiscoveryBatch", "DiscoveryObservation", "SourceAdapter"]
